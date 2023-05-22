a02fe5210872fbd3e65ca435f140217d0b5a13a8
Shubh Mall <mall.shubhkumar@gmail.com>
2023-05-22 10:06:28 -0700

    Finished footer implementation.

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt="logo" />
+          <p>Burnaby BC, Canada. All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overview</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in Touch</h4>
+          <p>Burnaby BC, Canada, V5A 0E1</p>
+          <p>666-777-8888</p>
+          <p>hire@me.pls</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>© 2023 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
